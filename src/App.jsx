2354bc6d51c658682beb2256a3c87ecd34b2ae92
Narendra-Simha <narendrasimhamekala18@gmail.com>
2024-10-12 12:03:34 +0530

    First submit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navigation from './components/navigation';
+import HeroSection from './components/hero';
+const App=()=>{
+    return (<div className="d">
+       <Navigation/>
+       <HeroSection/>
+    </div>
+    );
+}
+
+export default App;
